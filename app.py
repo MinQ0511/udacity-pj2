e90988c018c03947f8747d3c8155cc39d8cfd799
MinQ0511 <minq.0511@gmail.com>
2023-07-10 17:15:28 +0700

    #GET_PASSES_THIS_REPO_UDACITY_PLEASE

--- app.py
+++ app.py
@@ -45,4 +45,7 @@ def predict():
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    try:
+        app.run(host='0.0.0.0', port=5000, debug=True)
+    except Exception as e: 
+       print(e)
